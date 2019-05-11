b1afb50d19826309fcb0a5e82ed291e797e132fd
Javier Melendrez <javimelendrez@icloud.com>
2019-05-10 20:03:49 -0700

    added starter code

---
+++ LoopSyntax/LoopSyntax/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LoopSyntax
+//
+//  Created by Javier Melendrez on 5/10/19.
+//  Copyright © 2019 Javier Melendrez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
